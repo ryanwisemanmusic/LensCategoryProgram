eb65d68f64930631c6bd4d5e39c21d0b6dabac29
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-08-13 15:23:00 -0700

    LensApp V0.03
    Far from where I want my code, still needs to use array methods

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 
 using namespace std;
 
+ifstream iFile;
+ofstream oFile;
 //Lens Details contained in struct
 struct lensType
 {
     string lens;
-    string mount_type = "", brandName = "", 
+    string lensname = "", mount_type = "", brandName = "", 
     sensor_type = "", mm_lenth = "", 
     aperaturestop = ""; 
     double cost;
@@ -28,19 +30,79 @@ void printLensData(lensType lens);
 
 int main()
 {
-    
+    lensType lens = getLensData();
+    printLensData(lens);
+    return 0;
 }
 
 lensType getLensData()
 {
+    lensType l;
+    cout << "Enter lens name: ";
+    cin >> l.lensname;
+    cout << "Enter mount type: ";
+    cin >> l.mount_type;
+    cout << "Enter brand name: ";
+    cin >> l.brandName;
+    cout << "Enter sensor type: ";
+    cin >> l.sensor_type;
+    cout << "Enter mm length";
+    cin >> l.mm_lenth;
+    cout << "Enter f/t-stop";
+    cin >> l.aperaturestop;
 
+    while (true)
+    {
+        cout << "Enter a cost";
+        cin >> l.cost;
+        /*If incorrect price or string is used,
+        return an error with the price*/
+        if (cin.fail() || l.cost < 0 || !l.cost)
+        {
+            cin.clear();
+            cin.ignore(256, '\n');
+            cout << "Invalid price!";
+        }
+        
+    }
+    
 }
 
-void getLensName()
+void getLensName(const string input, lensType &lens)
 {
-
+    cout << input;
+    cin >> lens.lensname;
 }
+void getMountType(const string input, lensType &lens)
+{
+    cout << input;
+    cin >> lens.mount_type;
+}
+void getBrandName(const string input, lensType &lens)
+{
+    cout << input;
+    cin >> lens.brandName;
+}
+void getSensorType(const string input, lensType &lens)
+{
+    cout << input;
+    cin >> lens.sensor_type;
+}
+void get_mm_Length(const string input, lensType &lens)
+{
+    cout << input;
+    cin >> lens.mm_lenth;
+}
+void getAperatureStop(const string input, lensType &lens)
+{
+    cout << input;
+    cin >> lens.aperaturestop;
+}
+
+//Need to rework this with arrays since I want multiple output files. 
 void printLensData(const lensType lens)
 {
-    cout << "\nLens Data: ";
+    getLensName, getMountType, getBrandName,
+    getSensorType, get_mm_Length, getAperatureStop;
+    oFile << "output.txt";
 }
